c0238d4e43c880668d03176f86dd3b9e7d49e5ac
Zero-Trust <62760177+Zero-Trust@users.noreply.github.com>
2020-08-31 15:24:23 +0900

    initial code

---
+++ src/conf/config.js
@@ -0,0 +1,21 @@
+export const config = {
+  dev: {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appID: "app-id",
+  },
+  prod: {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appID: "app-id",
+  }
+};
+
